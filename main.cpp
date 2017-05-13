826e4fc4dd8d5e48f8fee2042967c872999cb03d
Bartosz <bartosz347@gmail.com>
2017-05-13 16:43:55 +0200

    displaying BMP image

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include "config.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  std::cout << "test";
 
-  return 0;
+    sf::Image BMPimage;
+    BMPimage.loadFromFile("../input_big.bmp");
+
+    sf::Vector2u BMPsize= BMPimage.getSize();
+    sf::RenderWindow window(sf::VideoMode(BMPsize.x, BMPsize.y),"MeanFilter");
+
+    sf::Texture imageTexture;
+    imageTexture.loadFromImage(BMPimage);
+    sf::Sprite imageSprite(imageTexture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        imageTexture.loadFromImage(BMPimage);
+        window.clear();
+        window.draw(imageSprite);
+        window.display();
+    }
+
+    return 0;
 }
