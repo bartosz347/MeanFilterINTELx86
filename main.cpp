ab360b556f15a65792f5f1654e3a15f1dc3ba12f
Bartosz <bartosz347@gmail.com>
2017-06-09 20:30:16 +0200

    minor cleanup

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include <SFML/Graphics.hpp>
 #include "mean_filter_cpp_ugly.h"
 
-#define USE_ASSEMBLER_VERSION 1
+//#define USE_CPP_VERSION
 
 using namespace std;
 
 extern "C" int mean_filter(int width, int height, int window, const sf::Uint8 *inImg, const sf::Uint8 *outImg);
-
 
 int main(int argc, char *argv[])
 {
@@ -20,7 +19,7 @@ int main(int argc, char *argv[])
     std::cout << "Podaj rozmiar okna (liczba nieparzysta <3-15>)" << std::endl;
     std::cin >> inputWindow;
 
-    if(inputWindow > 15 || inputWindow % 2 == 0) {
+    if (inputWindow > 15 || inputWindow % 2 == 0) {
         std::cout << "nieprawidlowy rozmiar okna" << std::endl;
         return -1;
     }
@@ -41,24 +40,23 @@ int main(int argc, char *argv[])
     imageTexture.loadFromImage(outImg);
     sf::Sprite imageSprite(imageTexture);
 
-    if (USE_ASSEMBLER_VERSION) {
-        mean_filter(srcImgSize.x, srcImgSize.y, inputWindow, srcImg.getPixelsPtr(), outImg.getPixelsPtr());
-    } else {
-        mean_filter_cpp(srcImgSize.x, srcImgSize.y, inputWindow, srcImg, outImg);
-    }
-
+#ifdef USE_CPP_VERSION
+    mean_filter_cpp(srcImgSize.x, srcImgSize.y, inputWindow, srcImg, outImg);
+#else
+    mean_filter(srcImgSize.x, srcImgSize.y, inputWindow, srcImg.getPixelsPtr(), outImg.getPixelsPtr());
+#endif
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-
-            imageTexture.loadFromImage(outImg);
-            window.clear();
-            window.draw(imageSprite);
-            window.display();
         }
+        imageTexture.loadFromImage(outImg);
+        window.clear();
+        window.draw(imageSprite);
+        window.display();
+
     }
 
     outImg.saveToFile("../output.bmp");
